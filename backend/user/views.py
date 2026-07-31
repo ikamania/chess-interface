68ef754807c5049dc3125bed8ca2f9f2b4f7834a
irakli <maniaika07@gmail.com>
2026-07-31 23:51:34 +0400

    feat(backend): add authentication endpoints

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,1 +1,35 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
+from rest_framework.views import APIView, Response
+from .serializers import RegisterSerializer, UserSerializer
+
+User = get_user_model()
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "username": request.user.username,
+            "email": request.user.email,
+        })
